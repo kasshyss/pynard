a200746f3a3dc106d3b1b8cfd8ccce9a32cb205d
meriadoc <pierre@bourgain.net>
2017-08-29 11:06:22 +0200

    add all the functionnalities into the main

--- pynard.py
+++ pynard.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python
 
-import modules.create_bottle
-
 def menu ():
     print "\n"
     print "------------------------------------"
     print "Welcome in pynard, your cave manager"
     print "What do you want to do ?"
-    print "1 - add a new bottle"
+    print "1 - add a bottle(s)"
+    print "2 - drink a bottle(s)"
+    print "3 - check my stock"
+    print "4 - bottle which need to be drink"
     print "0 - Exit"
     return raw_input("Enter your choice : ")
 
 
 def act(action):
     if action == "1":
-        print "action 1"
+        print "add to the stock"
+    elif action == "2":
+        print "remove bottle(s) from the stock"
+    elif action == "3":
+        print "display all / a subset"
+    elif action == "4":
+        print "display bottles which need to be drink"
     elif action == "0":
         print "Exit pynard"
     else:
