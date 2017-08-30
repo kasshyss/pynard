bf7625d1122f338e18a59ca6dd36b90fa0b5d345
meriadoc <pierre@bourgain.net>
2017-08-30 11:54:18 +0200

    add new files html, update the main with these html files

--- appli/pynard.py
+++ appli/pynard.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from flask import Flask, render_template
-import m_add as ad
+
 
 app = Flask(__name__)
 
@@ -10,38 +10,34 @@ app = Flask(__name__)
 @app.route('/index.html/')
 @app.route('/home/')
 def index():
-    toto = 'toto'
-    return render_template('index.html')    
-
-#def menu ():
-#    print "\n"
-#    print "------------------------------------"
-#    print "Welcome in pynard, your cave manager"
-#    print "What do you want to do ?"
-#    print "1 - add a bottle(s)"
-#    print "2 - drink a bottle(s)"
-#    print "3 - check my stock"
-#    print "4 - bottle which need to be drink"
-#    print "0 - Exit"
-#    return raw_input("Enter your choice : ")
+    title="Welcome in pynard, your cave manager"
+    page_title = "Pynard"
+    options_label = ["Add bottles","Remove bottles","Display the stock","Display the bottles which nee to be drink"]
+    return render_template('index.html', options_label = options_label, title = title, page_title = page_title)    
 
 
-#def act(action):
-#    if action == "1":
-#        ad.add_bottles(raw_input("How many bottle do you need to add? "))
+
+
+
+
+
+
+
+
+
+
 #    elif action == "2":
 #        print "remove bottle(s) from the stock"
-#    elif action == "3":
-#        print "display all / a subset"
-#    elif action == "4":
-#        print "display bottles which need to be drink"
-#    elif action == "0":
-#        print "Exit pynard"
-#    else:
-#        print "Wrong input"
 
-#action = -1
 
-#while action != "0":
-#    action = menu()
-#    act(str(action))
+
+
+
+
+
+
+
+
+
+
+
