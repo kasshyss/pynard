257dfd35cf98dd33671588cd75df6b99e67470d7
Meriadoc <pierre@bourgain.net>
2017-09-04 14:06:11 +0200

    remove print

--- appli/m_get.py
+++ appli/m_get.py
@@ -6,7 +6,6 @@ import os
 
 #return 
 def get_cave():
-    print 'toto'
     try:
         file = open(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + 'data' + os.path.sep + 'cave.csv', 'r')
         lines = file.readlines()
